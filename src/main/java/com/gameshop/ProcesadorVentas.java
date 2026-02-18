0b7737405948e769c58a7367bc4d7b0944df930f
Luis <lrojmor315@g.educaand.es>
2026-02-19 00:10:29 +0100

    Refactor: Metodo de IVA extraído y nombres profesionales

--- src/main/java/com/gameshop/ProcesadorVentas.java
+++ src/main/java/com/gameshop/ProcesadorVentas.java
@@ -4,10 +4,10 @@ public class ProcesadorVentas {
     private static final double DESCUENTO_VIP = 0.15;
     private static final double IVA = 1.21;
 
-    // VULNERABILIDAD SONARLINT: Contraseña en texto plano
-    private String dbClave = "admin_super_secreto_123";
 
-    public double c(double precioBase, boolean esVip) {
+
+
+    public double calcularPrecioFinal(double precioBase, boolean esVip) {
         double d = 0;
 
         if (esVip) {
@@ -16,11 +16,14 @@ public class ProcesadorVentas {
         }
 
         // BUG LÓGICO: Al VIP se le está SUMANDO el descuento al precio original, encareciendo el juego.
-        double pf = precioBase - d;
+        double PrecioTotal = precioBase - d;
 
         // CODE SMELL: Magic Number (1.21 representa el 21% de IVA)
-        pf = pf * IVA;
+        PrecioTotal = PrecioTotal * IVA;
 
-        return pf;
+        return PrecioTotal;
+        private double aplicarIva(double precio) {
+            return precio * IVA;
+        }
     }
 }
