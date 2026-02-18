581014c049a578a219f9880bfe2975a806af15c3
JoseMurciano <jose4murciano@gmail.com>
2026-02-19 00:25:00 +0100

    Fix: Corrección final de errores lógicos y cierre de tarea. Closes #4

--- src/main/java/com/gameshop/SistemaRecompensasVip.java
+++ src/main/java/com/gameshop/SistemaRecompensasVip.java
@@ -2,6 +2,9 @@ package com.gameshop;
 
 public class SistemaRecompensasVip {
 
+    public static final int PUNTOS_NIVEL_ORO = 100;
+    public static final int PUNTOS_NIVEL_LEYENDA = 500;
+
     public boolean comprobarVip(String tipoCliente) {
         // BUG (NPE): Si 'tipoCliente' es null (como pasa en el Main), intentar hacer .equals() lanza un NullPointerException.
         // SOLUCIÓN SONARLINT: Escribir "Premium".equals(tipoCliente)
@@ -22,8 +25,8 @@ public class SistemaRecompensasVip {
 
         // CODE SMELL: Complejidad Cognitiva alta (Escalera de Ifs)
         if (puntos > 0) {
-            if (puntos >= 100) {
-                if (puntos >= 500) {
+            if (puntos >= PUNTOS_NIVEL_ORO) {
+                if (puntos >= PUNTOS_NIVEL_LEYENDA) {
                     r = r + " ¡CLIENTE LEYENDA!";
                 } else {
                     r = r + " ¡CLIENTE ORO!";
